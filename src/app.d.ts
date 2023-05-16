5a124f29a99ba80f8795a25832bf224eec1c7c08
Danny Lewis <70882278+danny-people-plus@users.noreply.github.com>
2023-05-16 15:20:41 +0100

    Tidy (#127)
    * add tailwindcss prettier plugin to sort classes (#114)
    
    * add vscode extension recommendations (#115)
    
    * use cloudflare workers types for platform context (#116)
    
    * use cloudflare workers types for platform context
    
    * use latest version of worker types
    
    * test in multiple browsers and also on mobile (#121)
    
    ---------
    
    Co-authored-by: James Birtles <james@birtles.dev>

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,11 +1,13 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+
+import type { ExecutionContext, CacheStorage } from '@cloudflare/workers-types/2022-11-30';
+
 declare namespace App {
 	interface Platform {
-		context: {
-			waitUntil(promise: Promise<never>): void;
-		};
-		caches: CacheStorage & { default: Cache };
+		env: unknown;
+		context: ExecutionContext;
+		caches: CacheStorage;
 	}
 }
 
