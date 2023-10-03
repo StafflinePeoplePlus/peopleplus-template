b283421676adc92b863b26c1f3cba14c8fe2f3d1
mashaibani <Mohamed.Ashaibani@peopleplus.co.uk>
2023-10-03 15:39:18 +0100

    add dotenv + ci config for playwright tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,25 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 import { devices } from '@playwright/test';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const config: PlaywrightTestConfig = {
 	testDir: './playwright_tests',
 	testMatch: /.*.spec.ts/,
+	forbidOnly: !!process.env.CI,
+	workers: process.env.CI ? 1 : '75%',
+	retries: process.env.CI ? 3 : 0,
+	reporter: process.env.CI ? [['html'], ['github']] : undefined,
+	fullyParallel: true,
 	webServer: {
 		command: 'npm run build && npm run preview',
-		port: 4173
+		port: 4173,
+		reuseExistingServer: true,
+		timeout: 180 * 1000
+	},
+	use: {
+		trace: 'on-first-retry'
 	},
 	projects: [
 		{
