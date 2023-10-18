75cede97420db1f26c16703df9c9de214698337d
James Birtles <James.Birtles@peopleplus.co.uk>
2023-10-18 10:36:13 +0100

    Declare app namespace in global context (#277)
    Signed-off-by: James Birtles <James.Birtles@peopleplus.co.uk>

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,13 +1,15 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 
-import type { ExecutionContext, CacheStorage } from '@cloudflare/workers-types/2022-11-30';
+import type { ExecutionContext, CacheStorage } from '@cloudflare/workers-types';
 
-declare namespace App {
-	interface Platform {
-		env: unknown;
-		context: ExecutionContext;
-		caches: CacheStorage;
+declare global {
+	namespace App {
+		interface Platform {
+			env: unknown;
+			context: ExecutionContext;
+			caches: CacheStorage;
+		}
 	}
 }
 
